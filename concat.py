50b81c7ae47bcb26ca2348c3f92971ba03440d9c
aritche <ari.tchetchenian@gmail.com>
2020-11-23 16:03:17 +1100

    replaced with batch version

--- concat.py
+++ concat.py
@@ -1,14 +1,18 @@
+#############
+# Concatenate multiple trk files into a single trk file
+#############
+
 from dipy.io.streamline import load_tractogram, save_tractogram
 from dipy.io.stateful_tractogram import Space, StatefulTractogram
 import numpy as np
+from glob import glob
 
-fnA = './ISMRM_2015_Tracto_challenge_ground_truth_bundles_TCK_v2/CST_left.trk'
-A = load_tractogram(fnA, 'same')
+fns = glob('/home/aritche/Desktop/ismrm_2015_tractography_challenge_scoring/ISMRM_2015_Tracto_challenge_ground_truth_bundles_TCK_v2/*.trk')
 
-fnB = './ISMRM_2015_Tracto_challenge_ground_truth_bundles_TCK_v2/CST_right.trk'
-B = load_tractogram(fnB, 'same')
+concat = load_tractogram(fns[0], 'same')
+for fn in fns[1:]:
+    new_trk = load_tractogram(fn, 'same')
+    concat.streamlines.extend(new_trk.streamlines)
 
-A.streamlines.extend(B.streamlines)
-concat = StatefulTractogram(A.streamlines, fnA, Space.RASMM)
-
-save_tractogram(concat, 'concat.trk')
+tractogram_to_save = StatefulTractogram(concat.streamlines, fns[0], Space.RASMM)
+save_tractogram(tractogram_to_save, 'concat.trk')
